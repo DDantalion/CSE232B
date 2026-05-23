2f60505eccd279ff4d1a6f0ee771c2770290263a
Dantalion <hxiangyu@ucsc.edu>
2026-05-23 13:49:46 -0700

    debug

--- server.sh
+++ server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd /
 GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone https://github.com/DDantalion/CSE232B.git
-git checkout RRIP
 cd CSE232B
+git checkout RRIP
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
